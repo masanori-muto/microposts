14cac4593a1277683ddb456a518ae19038263f4e
masanori_muto <muto@memoffee.jp>
2016-02-25 08:04:39 +0000

    add profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def show # 6.7で追加
-    @user = User.find(params[:id])
   end
   
   def new
@@ -13,13 +15,32 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # 6.7で追加
     else
-      render 'new'
+      #登録できなかった時
+      flash.now[:alert] = "ユーザー登録に失敗しました"
+      render 'edit'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
     end
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area, :profile)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
